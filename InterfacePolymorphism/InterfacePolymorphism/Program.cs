81355061f9fdcc8b63547ba9336ba41adf2a4c9d
LeeJongCheol <leejongcheol2018@naver.com>
2018-12-30 17:51:40 +0900

    C# 인터페이스를 이용한 다형성 예제

---
+++ InterfacePolymorphism/InterfacePolymorphism/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+public interface Shape
+{
+    void write();
+    void Draw();
+}
+class Circle : Shape
+{
+    public void Draw()
+    {
+        // Code to draw a circle...
+        Console.WriteLine("원을 그리다");
+    }
+    public void write()
+    {
+        Console.WriteLine("글씨를 쓰다");
+    }
+}
+class Rectangle : Shape
+{
+    public void Draw()
+    {
+        // Code to draw a rectangle...
+        Console.WriteLine("사각형을 그리다");
+    }
+    public void write()
+    {
+        Console.WriteLine("글씨를 쓰다");
+    }
+}
+class Triangle : Shape
+{
+    public void Draw()
+    {
+        // Code to draw a triangle...
+        Console.WriteLine("삼각형을 그리다");
+    }
+    public void write()
+    {
+        Console.WriteLine("글씨를 쓰다");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Draw d = new Draw();
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Rectangle());
+        shapes.Add(new Triangle());
+        shapes.Add(new Circle());
+        foreach (Shape s in shapes)
+        {
+            s.Draw();
+            s.write();
+        }
+    }
+}
+/*사각형을 그리다
+글씨를 쓰다
+삼각형을 그리다
+글씨를 쓰다
+원을 그리다
+글씨를 쓰다    */
