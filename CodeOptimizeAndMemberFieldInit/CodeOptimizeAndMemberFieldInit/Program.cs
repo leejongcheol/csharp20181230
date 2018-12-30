8ae22fc95a339f241b072115219a0c73daa26d51
LeeJongCheol <leejongcheol2018@naver.com>
2018-12-30 17:41:52 +0900

    객체를 생성한 후 멤버들을 초기화 할 때 Setter 또는 프로퍼티를 직접 호출하여 초기화 할 수 있지만 C# 3.0 이후 가독성과 코드 최적화 등을 위한 생성시 멤버변수, 프로퍼티를 초기화하는 것이 가능해졌다

---
+++ CodeOptimizeAndMemberFieldInit/CodeOptimizeAndMemberFieldInit/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApplication10
+{
+    class Emp
+    {
+        public int Empno
+        {
+            get;
+            set;
+        }
+        public string Ename
+        {
+            get;
+            set;
+        }
+        public override String ToString()
+        {
+            return "[사번 : " + Empno + ", 이름 : " + Ename + "]";           
+ }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Emp e1 = new Emp();
+            e1.Empno = 1;
+            e1.Ename = "오라클자바커뮤니티";
+            Console.WriteLine(e1);
+            Emp e2 = new Emp()
+            {
+                Empno = 2,
+                Ename = "이종철"
+            };
+            Console.WriteLine(e2);
+        }
+    }
+}
+/*
+[결과]
+[사번: 1, 이름 : 오라클자바커뮤니티]
+[사번: 2, 이름 : 이종철]*/
