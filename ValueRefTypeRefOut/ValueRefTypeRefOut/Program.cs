71099f6dc142cdae839b5317bed6c05478f2f274
LeeJongCheol <leejongcheol2018@naver.com>
2018-12-30 17:59:23 +0900

    값 전달, 참조전달(ref, out), 참조전달을 위해서는 ref, out을 사용하면 되고 out은 값이 초기화 되어 있으면 안되고 ref는 초기화 되어 있어야한다.

---
+++ ValueRefTypeRefOut/ValueRefTypeRefOut/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+//아래 예제는 Swap 함수로 값 자체가 전달된다.
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Swap(int a, int b)
+        {
+            int tmp = a; a = b; b = tmp;
+        }
+        static void Main(string[] args)
+        {
+            int a = 10; int b = 20;
+            Console.WriteLine("a={0}, b={1}", a, b);
+            Swap(a, b);
+            Console.WriteLine("a={0}, b={1}", a, b);
+        }
+    }
+}
