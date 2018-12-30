124f42d35af79f3cd01108abc414976fb3ac52fc
LeeJongCheol <leejongcheol2018@naver.com>
2018-12-30 17:57:32 +0900

    C# partial 클래스 C# 2.0 이상에서 partial class는 여러 개의 파일에 하나의 클래스를 분리하여 만들거나 하나 의 파일에 여러 클래스를 만들 수 있는 방법을 제공한다. 특히 윈폼 및 웹폼 등에서 자동으 로 만들어 주는 코드와 개발자가 작성하는 닷넷 코드를 분리하여 작성하도록 해주기 위해 사 용되며 컴파일러가 분리된 partial class를 하나의 클래스로 합쳐준다.

---
+++ PartialClass/PartialClass/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+namespace ConsoleApplication7
+{
+    partial class Emp
+    {
+        public void gotoOffice()
+        {
+            Console.WriteLine("출근합니다.");
+        }
+    }
+    partial class Emp
+    {
+        public void Work()
+        {
+            Console.WriteLine("일합니다.");
+        }
+    }
+    partial class Emp
+    {
+        public void getoffWork()
+        {
+            Console.WriteLine("퇴근합니다.");
+        }
+    }
+    class MainTest
+    {
+        static void Main()
+        {
+            Emp e = new Emp();
+            e.gotoOffice();
+            e.Work();
+            e.getoffWork();
+        }
+    }
+}
+
+
+/*
+ 서로 다른 파일에 Emp 클래스를 나누어서 정의하는 경우
+[Program.cs]
+using System;
+namespace ConsoleApplication2
+{
+ partial class Emp
+ {
+ public int id;
+ public string name;
+ }
+ class Program
+ {
+ static void Main()
+ {
+ Emp e1 = new Emp();
+ e1.id = 1; e1.name = "1 길동";
+ Console.WriteLine(e1);
+ Console.WriteLine(e1.ToString());
+ Emp e2 = new Emp()
+ {
+ id = 2,
+ name = "2 길동"
+ };
+ Console.WriteLine(e2);
+ }
+ }
+}
+[Program2.cs]
+using System;
+namespace ConsoleApplication2
+{
+partial class Emp
+ {
+ public override String ToString()
+ {
+ return "[사원]id=" + id + ",name=" + name;
+ }
+ }
+}
+*/
