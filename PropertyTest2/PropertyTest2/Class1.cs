f7583de5c61b4a6081acb9254d56135ee6b1cfeb
LeeJongCheol <leejongcheol2018@naver.com>
2019-01-06 10:48:38 +0900

    속성 예제

---
+++ PropertyTest2/PropertyTest2/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace PropertyTest2
+{
+    class Emp : System.Object
+    {
+        private string name;   
+        public string Name  {  //일반속성
+            get         {                return this.name;            }
+            set          {                this.name = value + "님";          }
+        }
+        public int Sabun  //자동구현속성
+        {
+            get;
+            set;
+        }
+        public override string ToString()   {     return "[사원]" + Name + Sabun;      }
+    }
+    class EmpTest
+    {
+        static void Main()
+        {
+            Emp e = new Emp();  e.Name = "홍"; e.Sabun = 1004;
+            Console.WriteLine(e.Name);
+            Console.WriteLine(e);
+        }
+    }
+}
---
+++ PropertyTest2/PropertyTest2/Program.cs
@@ -0,0 +1,43 @@
+﻿/*using System;
+class EnumPropertyExam
+{
+    public enum DayOfWeek
+    {
+        Sunday = 0,
+        Monday = 1,
+        Tuesday = 2,
+        Wednesday = 3,
+        Thursday = 4,
+        Friday = 5,
+        Saturday = 6
+    }
+
+    DayOfWeek _day;
+    public DayOfWeek Day
+    {
+        get
+        {
+            // We don't allow this to be used on Friday.
+            if (this._day == DayOfWeek.Friday)
+            {
+                throw new Exception("Invalid access");
+            }
+            return this._day;
+        }
+        set
+        {
+            this._day = value;
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        EnumPropertyExam example = new EnumPropertyExam();
+        example.Day = DayOfWeek.Monday;
+        Console.WriteLine(example.Day == DayOfWeek.Monday);
+    }
+}
+*/
