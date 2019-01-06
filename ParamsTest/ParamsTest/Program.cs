58e471917715271a36d35ae144498ad230d99012
LeeJongCheol <leejongcheol2018@naver.com>
2019-01-06 10:00:43 +0900

    params,선택적인수,명명된인수 예제

---
+++ ParamsTest/ParamsTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace ParamsTest{
+    class Program    {
+        static void Main(string[] args)        {
+            Console.WriteLine(Sum3(j:1,i:2));  //명명된인수
+            Console.WriteLine(Sum3(j:1, i:2, k:3));  //명명된인수
+            Console.WriteLine(Sum());
+            Console.WriteLine( Sum(1,2) );
+            Console.WriteLine(new int[] { 1,2,3,4,5 });
+        }
+        static int Sum3(int i=0, int j=0, int k=0)  //선택적인수
+        {
+            Console.WriteLine("i={0}, j={1}, k={2}", i, j, k);
+            return i + j + k;
+        }
+        static int Sum(params int[] iArr)
+        {
+            int sum = 0;
+            foreach( int  num in  iArr   )
+            {
+                sum += num;
+            }
+            return sum;
+        }
+    }
+}
