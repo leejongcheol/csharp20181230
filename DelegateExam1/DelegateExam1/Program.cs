d9fa9ae43c2cae7b73850a58b21bef7e4aabd956
LeeJongCheol <leejongcheol2018@naver.com>
2019-01-13 14:50:48 +0900

    C# 이벤트, 사용자정의 이벤트, 이벤트 발생시 데이터 넘기기

---
+++ DelegateExam1/DelegateExam1/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ConsoleApplication11{
+    class Program    {
+        static void Main(string[] args)
+        {
+            Func<int, int, int> c1 = (i, j) => i + j;  //람다식, 무명함수,익명메소드            
+            Console.WriteLine($"3+4={c1(3, 4)}");
+        }      
+    }
+}
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello~! C#...");
+        }
+        private void a(object sender, EventArgs e)
+        {
+            MessageBox.Show("aHello~! C#...");
+        }
+        private void b(object sender, EventArgs e)
+        {
+            MessageBox.Show("bHello~! C#...");
+        }
+
+    }
+}
