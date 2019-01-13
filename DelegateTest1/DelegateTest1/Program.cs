28d6e6d91d673db8b8e561ca3435a8ac9aab992e
LeeJongCheol <leejongcheol2018@naver.com>
2019-01-13 12:01:25 +0900

    델리게이트 예제

--- DelegateTest1/DelegateTest1/Program.cs
+++ DelegateTest1/DelegateTest1/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 public class Delegate1
 {
-    //Delegate 선언은 Action, Finc로 대체할수 있다.
+    //델리게이트 선언은 Action, Func로 대체할수 있다.
     //private delegate int OnjSum(int i, int j); //1. 선언
     //private delegate void OnjSum2(int i, int j); //1. 선언
     public static void Main(string[] args)
@@ -9,12 +9,14 @@ public class Delegate1
         Delegate1 d1 = new Delegate1();
 
         //Sum이 static 이 아니므로 객체참조가 필요
-        //Func는 값을 리턴하는 메소드를 참조하는 시샵에서 제공하는 델리게이트로 16개 입력인자 가질수있다.
-        Func<int, int,  int> myMethod = d1.Sum; //2. 생성, 메소드이름을 인자로
+        //Func는 값을 리턴하는 메소드를 참조하는 
+        //시샵에서 제공하는 델리게이트로 16개 입력인자 가질수있다.
+        Func<int, int,  int> myMethod = d1.Sum;               //2. 생성, 메소드이름을 인자로
         Console.WriteLine("두수 합 : {0}", myMethod(10, 30));  //3. 실행
 
-        //Action은 값을 리턴하지않는 메소드를 참조하는 델리게이트로 시샵에서 인자17까지 제공함
-        //Sum3가 static 이므로 다이렉트 접근, 객체생성없이 접근
+        //Action은 값을 리턴하지않는 메소드를 참조하는 델리게이트
+        //시샵에서 인자17까지 제공한다.
+        //Sum3가 static 이므로 다이렉트 접근, 객체 생성없이 접근
         Action<int, int> d2 =Sum2;
         d2(10, 20);
     }
